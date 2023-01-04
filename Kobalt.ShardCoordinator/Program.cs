b569763abc8197ad3cdddc7a2ee9c7557ace66ba
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-01-04 18:56:10 -0500

    Slightly more functional. Sessions work for the most part.

--- Kobalt.ShardCoordinator/Program.cs
+++ Kobalt.ShardCoordinator/Program.cs
@@ -2,15 +2,32 @@ using System.Net;
 using System.Net.WebSockets;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Kobalt.Infrastructure.Types;
 using Kobalt.ShardCoordinator.Services;
+using Kobalt.ShardCoordinator.Types;
 using Microsoft.AspNetCore.Authentication;
+using Remora.Discord.API.Abstractions.Rest;
+using Remora.Discord.Caching.Extensions;
+using Remora.Discord.Rest;
+using Remora.Discord.Rest.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDiscordRest(s => (s.GetRequiredService<IConfiguration>()["Discord:Token"], DiscordTokenType.Bot));
+builder.Services.AddDiscordCaching();
+
+builder.Services.Configure<KobaltConfig>(builder.Configuration);
+
+builder.Services
+       .AddSingleton<SessionManager>()
+       .AddSingleton<WebsocketManagerService>();
+
+await SetMaxConcurrency(builder);
+
 var app = builder.Build();
+
 app.UseHttpsRedirection();
 app.MapControllers();
 
@@ -18,29 +35,52 @@ app.UseWebSockets();
 
 app.MapPost
 (
-    "/shards", async (HttpContext Context, SessionManager Manager) =>
+    "/shards", async (HttpContext context, SessionManager manager) =>
     {
-        var shardResult = await Manager.GetNextAvailableSessionAsync();
+        var sessionResult = await manager.GetNextAvailableSessionAsync();
 
-        if (!shardResult.IsDefined(out var shard))
+        if (!sessionResult.IsDefined(out var session))
         {
             // 409 because we're out of shards; try again in ~20 seconds
-            Context.Response.StatusCode = 409;
-            Context.Response.Headers["X-Retry-After"] = "20000";
+            context.Response.Headers["X-Retry-After"] = "20000";
+            return Results.Conflict();
         }
         else
         {
             // 201 is "Created", though it's not gaurenteed to be a new session.
-            Context.Response.StatusCode = 201;
-            Context.Response.WriteAsJsonAsync
-            (
-                shard, 
-                new JsonSerializerOptions()
-                {
-                    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-                }
-            );
+            return Results.Created($"/shards/{session.ShardID}", session);
         }
+    }
+);
+
+app.MapDelete
+(
+    "/shards/{shardID}", async (HttpContext context, int shardID, WebsocketManagerService websocketManager, SessionManager sessions) =>
+    {
+        var shardId = context.Request.RouteValues["id"] as int?;
+        
+        var sessionId = context.Request.Headers["X-Session-ID"].ToString();
+        
+        if (!sessions.IsValidSession(sessionId, shardId, out var parsedSessionId))
+        {
+            return Results.Unauthorized();
+        }
+        
+        var body = await JsonSerializer.DeserializeAsync<(string gatewaySessionID, int? sequence)>(context.Request.Body);
+        
+        if (string.IsNullOrEmpty(body.gatewaySessionID) || body.sequence is not {} gatewaySequence)
+        {
+            return Results.BadRequest();
+        }
+
+        var sessionResult = await websocketManager.TerminateClientSessionAsync(parsedSessionId, body.gatewaySessionID, gatewaySequence);
+        
+        if (!sessionResult.IsSuccess)
+        {
+            return Results.NotFound();
+        }
+
+        return Results.NoContent();
     }
 );
 
@@ -68,3 +108,20 @@ app.Use(async (context, next) =>
 });
 
 app.Run();
+
+
+// Cursed, but works.
+static async Task SetMaxConcurrency(WebApplicationBuilder webApplicationBuilder)
+{
+    var tempServices = webApplicationBuilder.Services.BuildServiceProvider();
+    var gateway = tempServices.GetRequiredService<IDiscordRestGatewayAPI>();
+
+    var gatewayInfoResult = await gateway.GetGatewayBotAsync();
+
+    if (!gatewayInfoResult.IsSuccess)
+    {
+        throw new Exception("Failed to get gateway info");
+    }
+
+    var concurrency = gatewayInfoResult.Entity.SessionStartLimit.Value.MaxConcurrency;
+}
