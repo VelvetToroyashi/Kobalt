f51184a6d07565bda937563a690b06f9468857c9
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-02-23 18:13:41 -0500

    Cleanup & start of new project

--- zirconium/src/api.rs
+++
@@ -1,8 +0,0 @@
-use rocket::{put, delete, Rocket, State, routes};
-use std::sync::Mutex;
-
-pub fn create_rocket() -> Rocket {
-    Rocket::ignite()
-        .mount("/", routes![put, delete])
-        .manage(Mutex::new(0))
-}
