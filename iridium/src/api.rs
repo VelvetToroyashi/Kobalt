28df892e9a5b48ecd771f5ba0bb74418dedc5b5c
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2023-03-04 11:03:02 -0500

    Beginning of API, h

--- iridium/src/api.rs
+++ iridium/src/api.rs
@@ -1,18 +1,36 @@
-use rocket::{get, put, delete, Rocket, routes};
+use std::collections::HashMap;
+use axum::{Router, routing::post};
+use axum::extract::Query;
+use axum::response::IntoResponse;
+use serde::Deserialize;
+use crate::discord::DiscordUser;
 
 pub struct Api;
 
-impl Api {
-    pub fn new() -> Api {
-        Api
-    }
+#[derive(Clone)]
+pub(crate) struct Config {
+    pub bot_token: String,
+}
 
-    pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
-        let res = rocket::build()
-            .mount("/", routes![])
-            .launch()
+impl Api {
+    pub async fn run() -> Result<(), Box<dyn std::error::Error>> {
+        let bot_token = std::env::var("BOT_TOKEN").expect("BOT_TOKEN must be set");
+
+        let app = Router::new()
+            .route("/phishing/check/image", post(check_image))
+            .with_state(Config { bot_token });
+
+        axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+            .serve(app.into_make_service())
             .await?;
+
+
 
         Ok(())
     }
+}
+
+// Write an API endpoint (POST /check/image) that takes an avatar hash as a query parameter, and requires authentication.
+async fn check_image(Query(hash): Query<HashMap<String, String>>, _user: DiscordUser) -> impl IntoResponse {
+    format!("Hello, world! {}", hash["hash"])
 }
