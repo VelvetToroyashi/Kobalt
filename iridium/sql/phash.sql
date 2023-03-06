dc16fdadcf77e6e54619de9cf8da62c438d56285
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-03-06 04:18:15 -0500

    It builds :yippee:

--- iridium/sql/phash.sql
+++ iridium/sql/phash.sql
@@ -1,3 +1,5 @@
-SELECT * FROM images
-    ORDER BY phash <-> $1 ASC
-    LIMIT 1
+SELECT *
+    FROM images
+    WHERE phash <-> $1 < $2
+    ORDER BY phash <-> $1 DESC -- Is there a better way to do this?
+    LIMIT 1;
