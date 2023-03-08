10aca93257fd6764a76483372ebc842452b7d592
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2023-03-08 12:46:40 -0500

    Schema-per-service

--- iridium/migrations/2023-02-25-091012_initial_iridium_setup/up.sql
+++ iridium/migrations/2023-02-25-091012_initial_iridium_setup/up.sql
@@ -1,12 +1,12 @@
--- Your SQL goes here
-
 -- Create a postgres table called "images" that has an auto-incrementing ID, a category, a hash, and when it was updated
-CREATE EXTENSION pg_trgm; -- For fuzzy string matching
+CREATE EXTENSION IF NOT EXISTS pg_trgm; -- For fuzzy string matching
 
-CREATE TABLE images (
+CREATE SCHEMA iridium;
+
+CREATE TABLE iridium.images (
     id SERIAL PRIMARY KEY,
     category TEXT NOT NULL, -- "discord_modmail_nuggies", or the likes
-    source TEXT NOT NULL, -- Discord, Twitter, etc.
+    source TEXT NOT NULL UNIQUE, -- Discord, Twitter, etc.
     added_by BIGINT NOT NULL, -- Discord ID of the person who added it
     added TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
     md5_hash TEXT UNIQUE, -- Discord uses MD5 hashes for their image URLs
--- iridium/src/schema.rs
+++ iridium/src/schema.rs
@@ -1,13 +1,15 @@
 // @generated automatically by Diesel CLI.
 
-diesel::table! {
-    images (id) {
-        id -> Int4,
-        category -> Text,
-        source -> Text,
-        added_by -> Int8,
-        added -> Timestamp,
-        md5_hash -> Nullable<Text>,
-        phash -> Bytea,
+pub mod iridium {
+    diesel::table! {
+        iridium.images (id) {
+            id -> Int4,
+            category -> Text,
+            source -> Text,
+            added_by -> Int8,
+            added -> Timestamp,
+            md5_hash -> Nullable<Text>,
+            phash -> Bytea,
+        }
     }
 }
