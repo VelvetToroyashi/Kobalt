d7c911d3bf2c4caeec27e311c1a65554f8710594
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-03-08 11:58:46 -0500

    Kablow

--- iridium/src/main.rs
+++ iridium/src/main.rs
@@ -1,20 +1,22 @@
-#![feature(decl_macro)]
+//#![feature(decl_macro)]
 extern crate core;
 
-pub mod models;
-pub mod schema;
 mod api;
 mod discord;
+pub mod models;
+pub mod schema;
 
-use std::error::Error;
-use diesel::{backend::Backend};
+use diesel::backend::Backend;
+use diesel::{Connection, PgConnection};
 use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+use std::error::Error;
 
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
 
 #[allow(dead_code)]
-fn run_migrations<DB: Backend>(connection: &mut impl MigrationHarness<DB>) -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
-
+fn run_migrations<DB: Backend>(
+    connection: &mut impl MigrationHarness<DB>,
+) -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     // This will run the necessary migrations.
     //
     // See the documentation for `MigrationHarness` for
@@ -27,8 +29,11 @@ fn run_migrations<DB: Backend>(connection: &mut impl MigrationHarness<DB>) -> Re
 type Err = Box<dyn Error>;
 #[tokio::main]
 async fn main() -> Result<(), Err> {
-    api::Api::run().await?;
+    let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let mut connection = PgConnection::establish(&db_url)?;
+    run_migrations(&mut connection).map_err(|e| e as Err)?;
 
+    api::Api::run().await?;
 
     Ok(())
 }
