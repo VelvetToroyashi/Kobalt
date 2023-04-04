bbb91bc60405cbede675ce55d6ac99d5651e4558
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2023-04-04 17:08:53 -0400

    I'll come back to this

---
+++ Kobalt.Artists.API/API/FurAffinityAPI.cs
@@ -0,0 +1,50 @@
+﻿using AngleSharp;
+using Remora.Results;
+
+namespace Kobalt.Artists.API.Services;
+
+public class FurAffinityParser
+{
+    private readonly HttpClient _client;
+    
+    public FurAffinityParser(IHttpClientFactory client)
+    {
+        _client = client.CreateClient("FurAffinity");
+    }
+
+    public async Task<Result<string>> GetArtistBioAsync(string artist)
+    {
+        using var response = await _client.GetAsync("https://www.furaffinity.net/user/{artist}/");
+        
+        if (!response.IsSuccessStatusCode)
+        {
+            return Result<string>.FromError<HttpRequestException>(new HttpRequestException("Failed to query for the artist's profile. Is FurAffinity down?"));
+        }
+        
+        var html = await response.Content.ReadAsStringAsync();
+
+        if (html.Contains("Log In</strong>"))
+        {
+            return new InvalidOperationError("The credentials for FurAffinity are invalid.");
+        }
+
+        var context = BrowsingContext.New();
+        var document = await context.OpenAsync(req => req.Content(html));
+
+        var systemError = document.QuerySelector("h2");
+        
+        if (systemError?.TextContent == "System Error")
+        {
+            return new InvalidOperationError("The artist's profile either does not exist, or is disabled.");
+        }
+        
+        var bio = document.QuerySelector("div.section-body.userpage-profile");
+
+        if (bio is null)
+        {
+            return new InvalidOperationError("The artist's bio could not be retrieved. This could be a bug.");
+        }
+        
+        return bio.InnerHtml;
+    }
+}
---
+++ Kobalt.Artists.Data/Entities/Artist.cs
@@ -0,0 +1,11 @@
+﻿namespace Kobalt.Artists.Data.Entities;
+
+public class Artist
+{
+    public ulong       DiscordId        { get; set; }
+    public ulong?      VerifiedBy       { get; set; }
+    public ArtistFlags Flags            { get; set; }
+    public DateTime?   VerifiedAt       { get; set; }
+    public string?     TwitterHandle    { get; set; }
+    public string?     FurAffinityName  { get; set; }
+}
