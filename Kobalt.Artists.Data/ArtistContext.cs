425723de36a38a1fa335340b859c136da4fdea05
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-04-04 17:08:53 -0400

    I'll come back to this

---
+++ Kobalt.Artists.Data/ArtistContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Kobalt.Artists.Data;
+
+public class ArtistContext : DbContext
+{
+
+}
---
+++ Kobalt.Artists.Data/Entities/ArtistFlags.cs
@@ -0,0 +1,15 @@
+﻿namespace Kobalt.Artists.Data.Entities;
+
+/// <summary>
+/// Flags for an artist, such whether they've been verified or are known as an art scammer.
+/// </summary>
+[Flags]
+public enum ArtistFlags
+{
+    PendingVerification,
+    AutomaticallyVerified,
+    ManuallyVerified,
+    PreviouslyVerified,
+    MarkedAsSuspicious,
+    KnownFraudulentArtist,
+}
