100546b9f76c229a2fb3a909e9ca46d2bf39c48a
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-04-06 10:59:21 -0400

    feat(reminders): add ReminderCreationPayload

---
+++ Kobalt.Infrastructure/DTOs/Reminders/ReminderCreationPayload.cs
@@ -0,0 +1,8 @@
+﻿namespace Kobalt.Infrastructure.DTOs.Reminders;
+
+/// <summary>
+/// Represents the return result of creating a reminder.
+/// </summary>
+/// <param name="Id">The id of the reminder.</param>
+/// <param name="Expiration">When the reminder expires</param>
+public record ReminderCreationPayload(int Id, DateTimeOffset Expiration);
