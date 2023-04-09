bf001788f96f399f366a0782c9fc11383793c05e
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-04-09 01:28:22 -0400

    docs: Add desperately missing documentaiton for infractions

--- src/InfractionsAPI/Kobalt.Infractions.Shared/DTOs/InfractionDTO.cs
+++ src/InfractionsAPI/Kobalt.Infractions.Shared/DTOs/InfractionDTO.cs
@@ -2,6 +2,19 @@
 
 namespace Kobalt.Infractions.Infrastructure.Mediator.DTOs;
 
+/// <summary>
+/// Represents an infraction in the system.
+/// </summary>
+/// <param name="Id">The ID of the infraction.</param>
+/// <param name="ReferencedId">The ID of the referenced infraction, if applicable.</param>
+/// <param name="IsHidden">Whether this infraction is hidden.</param>
+/// <param name="Reason">The reason for this infraction.</param>
+/// <param name="UserID">The ID of the target user.</param>
+/// <param name="GuildID">The ID of the guild this infraction was created in.</param>
+/// <param name="ModeratorID">The ID of the moderator that created this infraction.</param>
+/// <param name="Type">The type of this infraction.</param>
+/// <param name="CreatedAt">The time this infraction was created.</param>
+/// <param name="ExpiresAt">When this infraction expires, if applicable.</param>
 public record InfractionDTO
 (
     int Id,
@@ -13,6 +26,5 @@ public record InfractionDTO
     ulong ModeratorID,
     InfractionType Type,
     DateTimeOffset CreatedAt,
-    DateTimeOffset? ExpiresAt,
-    bool IsUpdated = false
+    DateTimeOffset? ExpiresAt
 );
