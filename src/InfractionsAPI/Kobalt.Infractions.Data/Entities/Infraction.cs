8b05bf72f1cb88c5448e60a6363f0983f3ede8ac
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2023-04-09 01:28:22 -0400

    docs: Add desperately missing documentaiton for infractions

--- src/InfractionsAPI/Kobalt.Infractions.Data/Entities/Infraction.cs
+++ src/InfractionsAPI/Kobalt.Infractions.Data/Entities/Infraction.cs
@@ -4,26 +4,75 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Kobalt.Infractions.Data.Entities;
 
+/// <summary>
+/// Represents an infraction.
+/// </summary>
 public class Infraction
 {
+    /// <summary>
+    /// The ID of the infraction.
+    /// </summary>
     public int Id { get; set; }
+    
+    /// <summary>
+    /// The ID of the referenced infraction, if applicable.
+    /// </summary>
     public int? ReferencedId { get; set; }
     
+    /// <summary>
+    /// Whether this infraction is hidden.
+    /// </summary>
     public bool IsHidden { get; set; }
+    
+    /// <summary>
+    /// Whether this infraction is processable.
+    /// </summary>
+    // ?? What is this for?
     public bool IsProcessable { get; set; }
+
+    /// <summary>
+    /// The reason for this infraction.
+    /// </summary>
     public required string Reason { get; set; }
 
+    /// <summary>
+    /// The ID of the target user.
+    /// </summary>
     public required ulong UserID { get; set; }
+    
+    /// <summary>
+    /// The ID of the guild this infraction was created in.
+    /// </summary>
     public required ulong GuildID { get; set; }
+    
+    /// <summary>
+    /// The ID of the moderator that created this infraction.
+    /// </summary>
     public required ulong ModeratorID { get; set; }
     
+    /// <summary>
+    /// The type of this infraction.
+    /// </summary>
     public required InfractionType Type { get; set; }
     
+    /// <summary>
+    /// The time this infraction was created.
+    /// </summary>
     public required DateTimeOffset CreatedAt { get; set; }
+    
+    /// <summary>
+    /// When this infraction expires, if applicable.
+    /// </summary>
     public DateTimeOffset? ExpiresAt { get; set; }
     
+    /// <summary>
+    /// When the infraction was last updated, if ever.
+    /// </summary>
     public DateTimeOffset? LastUpdated { get; set; }
     
+    /// <summary>
+    /// Foreign key to the referenced infraction, if applicable.
+    /// </summary>
     public Infraction? Referenced { get; set; }
 }
 
