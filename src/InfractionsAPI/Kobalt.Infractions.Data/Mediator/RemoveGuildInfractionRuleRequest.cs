c58fe95b67cfb6a6063953e5b52c12045d6e515a
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-04-10 00:45:55 -0400

    feat(infractions): Add RemoveGuildInfractionRuleRequest

---
+++ src/InfractionsAPI/Kobalt.Infractions.Data/Mediator/RemoveGuildInfractionRuleRequest.cs
@@ -0,0 +1,37 @@
+﻿using Mediator;
+using Remora.Results;
+
+namespace Kobalt.Infractions.Data.Mediator;
+
+public record RemoveGuildInfractionRuleRequest(int Id, ulong GuildID) : IRequest<Result>;
+
+public class RemoveGuildInfractionRuleRequestHandler : IRequestHandler<RemoveGuildInfractionRuleRequest, Result>
+{
+    private readonly InfractionContext _context;
+
+    public RemoveGuildInfractionRuleRequestHandler(InfractionContext context)
+    {
+        _context = context;
+    }
+
+    public async ValueTask<Result> Handle(RemoveGuildInfractionRuleRequest request, CancellationToken ct = default)
+    {
+        var rule = await _context.InfractionRules.FindAsync(request.Id, ct);
+
+        if (rule is null)
+        {
+            return new NotFoundError("Infraction rule not found");
+        }
+
+        if (rule.GuildID != request.GuildID)
+        {
+            return new NotFoundError("Infraction rule not found");
+        }
+
+        _context.InfractionRules.Remove(rule);
+
+        await _context.SaveChangesAsync(ct);
+
+        return Result.FromSuccess();
+    }
+}
