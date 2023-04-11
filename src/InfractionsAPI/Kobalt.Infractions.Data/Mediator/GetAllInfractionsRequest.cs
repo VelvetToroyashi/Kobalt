2b0e21f39e187302757f21135e43a363a9d8d724
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-04-10 20:18:35 -0400

    fix(infractions): Load infractions in-memory during startup

---
+++ src/InfractionsAPI/Kobalt.Infractions.Data/Mediator/GetAllInfractionsRequest.cs
@@ -0,0 +1,41 @@
+﻿using Kobalt.Infractions.Infrastructure.Mediator.DTOs;
+using Mediator;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kobalt.Infractions.Data.Mediator;
+
+public record GetAllInfractionsRequest() : IRequest<IEnumerable<InfractionDTO>>;
+
+public class GetAllInfractionsHandlerRequest : IRequestHandler<GetAllInfractionsRequest, IEnumerable<InfractionDTO>>
+{
+    private readonly InfractionContext _context;
+
+    public GetAllInfractionsHandlerRequest(InfractionContext context)
+    {
+        _context = context;
+    }
+
+    public async ValueTask<IEnumerable<InfractionDTO>> Handle(GetAllInfractionsRequest request, CancellationToken cancellationToken)
+    {
+        return await _context
+                     .Infractions
+                     .Where(inf => inf.IsProcessable)
+                     .Select
+                     (
+                         x => new InfractionDTO
+                         (
+                             x.Id,
+                             x.ReferencedId,
+                             x.IsHidden,
+                             x.Reason,
+                             x.UserID,
+                             x.GuildID,
+                             x.ModeratorID,
+                             x.Type,
+                             x.CreatedAt,
+                             x.ExpiresAt
+                         )
+                     )
+                     .ToArrayAsync();
+    }
+}
