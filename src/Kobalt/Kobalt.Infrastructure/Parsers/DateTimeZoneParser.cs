a84718f319fbd1642ba71d43eb57b279d3d945f7
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-04-14 14:05:01 -0400

    refactor: Change OffsetParser to DateTimeZoneParser

--- src/Kobalt/Kobalt.Infrastructure/Parsers/DateTimeZoneParser.cs
+++ src/Kobalt/Kobalt.Infrastructure/Parsers/DateTimeZoneParser.cs
@@ -1,38 +1,39 @@
 ﻿using Kobalt.Shared.Extensions;
 using NodaTime;
+using NodaTime.TimeZones;
 using Remora.Commands.Parsers;
 using Remora.Commands.Results;
 using Remora.Results;
 
 namespace Kobalt.Infrastructure.Parsers;
 
-public class OffsetParser : AbstractTypeParser<Offset>
+public class DateTimeZoneParser : AbstractTypeParser<DateTimeZone>
 {
     private readonly IDateTimeZoneProvider _dateTimeZoneProvider;
 
-    public OffsetParser(IDateTimeZoneProvider dateTimeZoneProvider)
+    public DateTimeZoneParser(IDateTimeZoneProvider dateTimeZoneProvider)
     {
         _dateTimeZoneProvider = dateTimeZoneProvider;
     }
 
-    public override ValueTask<Result<Offset>> TryParseAsync(string token, CancellationToken ct = default)
+    public override ValueTask<Result<DateTimeZone>> TryParseAsync(string token, CancellationToken ct = default)
     {
         var tzInfoResult = ResultExtensions.TryCatch(() => TimeZoneInfo.FindSystemTimeZoneById(token));
      
         if (tzInfoResult.IsDefined(out var tzInfo))
         {
-            var offset = Offset.FromHours(tzInfo.GetUtcOffset(DateTimeOffset.UtcNow).Hours);
+            var offset = BclDateTimeZone.FromTimeZoneInfo(tzInfo);
             
-            return ValueTask.FromResult<Result<Offset>>(offset);
+            return ValueTask.FromResult<Result<DateTimeZone>>(offset);
         }
         
         var offsetName = _dateTimeZoneProvider.GetZoneOrNull(token);
         
         if (offsetName is null)
         {
-            return ValueTask.FromResult<Result<Offset>>(new ParsingError<Offset>("Invalid timezone."));
+            return ValueTask.FromResult<Result<DateTimeZone>>(new ParsingError<DateTimeZone>("Invalid timezone."));
         }
 
-        return ValueTask.FromResult<Result<Offset>>(offsetName.GetUtcOffset(Instant.FromDateTimeOffset(DateTimeOffset.UtcNow)));
+        return ValueTask.FromResult<Result<DateTimeZone>>(offsetName);
     }
 }
