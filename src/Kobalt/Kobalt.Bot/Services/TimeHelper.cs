bf0da5b7714bfb9767fbafc1160a98e9126d3fce
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-04-14 14:04:46 -0400

    feat: Add ZonedDateTimeParser and time helper class

---
+++ src/Kobalt/Kobalt.Bot/Services/TimeHelper.cs
@@ -0,0 +1,38 @@
+﻿using Kobalt.Shared.Extensions;
+using NodaTime;
+using NodaTime.TimeZones;
+using Remora.Commands.Results;
+using Remora.Results;
+
+namespace Kobalt.Bot.Services;
+
+/// <summary>
+/// A helper class for time-related operations.
+/// </summary>
+public static class TimeHelper
+{
+    /// <summary>
+    /// Attempts to parse a string into a <see cref="DateTimeZone"/>.
+    /// </summary>
+    /// <param name="input">The input string.</param>
+    /// <param name="provider">A provider of timezones.</param>
+    /// <returns>A result of the extracted timezone, otherwise an error.</returns>
+    public static Result<DateTimeZone> GetDateTimeZoneFromString(string input, IDateTimeZoneProvider provider)
+    {
+        var bclResult = ResultExtensions.TryCatch(() => TimeZoneInfo.FindSystemTimeZoneById(input));
+        
+        if (bclResult.IsDefined(out var bcl))
+        {
+            return BclDateTimeZone.FromTimeZoneInfo(bcl);
+        }
+        
+        var offsetName = provider.GetZoneOrNull(input);
+        
+        if (offsetName is null)
+        {
+            return new ParsingError<DateTimeZone>("Invalid timezone.");
+        }
+        
+        return offsetName;
+    }
+}
---
+++ src/Kobalt/Kobalt.Infrastructure/Parsers/ZonedDateTimeParser.cs
@@ -0,0 +1,79 @@
+﻿using Kobalt.Shared.Mediator.Users;
+using Mediator;
+using Microsoft.Recognizers.Text;
+using NodaTime;
+using Recognizers.Text.DateTime.Wrapper;
+using Recognizers.Text.DateTime.Wrapper.Models.BclDateTime;
+using Recognizers.Text.DateTime.Wrapper.Models.Enums;
+using Remora.Commands.Parsers;
+using Remora.Commands.Results;
+using Remora.Discord.Commands.Contexts;
+using Remora.Discord.Commands.Extensions;
+using Remora.Results;
+
+namespace Kobalt.Infrastructure.Parsers;
+
+public class ZonedDateTimeParser : AbstractTypeParser<ZonedDateTime>
+{
+    private static ISet<DateTimeV2Type> _supportedTypes = new HashSet<DateTimeV2Type>
+    {
+        DateTimeV2Type.Date,
+        DateTimeV2Type.DateTime,
+    };
+    
+    private readonly IMediator _mediator;
+    private readonly IOperationContext _context;
+    
+    public ZonedDateTimeParser(IMediator mediator, IOperationContext context)
+    {
+        _mediator = mediator;
+        _context = context;
+    }
+
+    public override async ValueTask<Result<ZonedDateTime>> TryParseAsync(string token, CancellationToken ct = default)
+    {
+        if (!_context.TryGetUserID(out var userID))
+        {
+            return new InvalidOperationError("Could not get user ID.");
+        }
+        
+        var userResult = await _mediator.Send(new GetUserRequest(userID.Value), ct);
+
+        var offset = userResult.Entity?.Timezone ?? Offset.Zero;
+        var extractionResult = ExtractDateTimeOffset(token, offset.ToTimeSpan());
+        
+        if (extractionResult.IsSuccess)
+        {
+            return ZonedDateTime.FromDateTimeOffset(extractionResult.Entity);
+        }
+        
+        return new ParsingError<ZonedDateTime>("Invalid date.");
+    }
+
+    private static Result<DateTimeOffset> ExtractDateTimeOffset(string input, TimeSpan offset)
+    {
+        var refTime = DateTime.UtcNow + offset;
+        var extractionResult = DateTimeV2Recognizer.RecognizeDateTimes(input, Culture.English, refTime, _supportedTypes);
+
+        if (extractionResult.FirstOrDefault() is not {} result || !result.Resolution.Values.Any())
+        {
+            return Result<DateTimeOffset>.FromError(new NotFoundError());
+        }
+
+        var res = result.Resolution
+              .Values
+              .Select(v => v is DateTimeV2Date dtd ? dtd.Value : v is DateTimeV2DateTime dtt ? dtt.Value : (DateTime?)null)
+              .FirstOrDefault
+              (
+                  v => v > refTime
+              );
+        
+        if (res is null)
+        {
+            return Result<DateTimeOffset>.FromError(new NotFoundError());
+        }
+        
+        return new DateTimeOffset(res.Value);
+    }
+
+}
