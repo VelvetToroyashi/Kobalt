cc27cabce57ff99265647e9fb8701b512f497f0e
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-04-22 02:39:44 -0400

    feat: Add kick command

---
+++ src/Kobalt/Plugins/Kobalt.Plugins.Infractions/Commands/KickCommand.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using Kobalt.Plugins.Infractions.Services;
+using Kobalt.Shared.Conditions;
+using Remora.Commands.Attributes;
+using Remora.Commands.Groups;
+using Remora.Discord.API.Abstractions.Objects;
+using Remora.Discord.API.Abstractions.Rest;
+using Remora.Discord.Commands.Conditions;
+using Remora.Discord.Commands.Contexts;
+using Remora.Results;
+
+namespace Kobalt.Plugins.Infractions.Commands;
+
+public class KickCommand : CommandGroup
+{
+    private readonly IInteractionContext _context;
+    private readonly InfractionAPIService _apiService;
+    private readonly IDiscordRestInteractionAPI _interactions;
+
+    public KickCommand(IInteractionContext context, InfractionAPIService apiService, IDiscordRestInteractionAPI interactions)
+    {
+        _context = context;
+        _apiService = apiService;
+        _interactions = interactions;
+    }
+
+    [Command("kick")]
+    [Description("Kicks a user from the guild.")]
+    [RequireContext(ChannelContext.Guild)]
+    public async Task<Result> KickAsync
+    (
+        [Description("The user to be kicked.")]
+        [EnsureHierarchy(HierarchyTarget.Self, HierarchyLevel.Higher)]
+        [EnsureHierarchy(HierarchyTarget.Invoker, HierarchyLevel.Higher)]
+        IUser target,
+
+        [Description("The reason for the kick.")]
+        string reason = "Not Given."
+    )
+    {
+        var result = await _apiService.KickUserAsync(_context.Interaction.GuildID.Value, target, _context.Interaction.Member.Value.User.Value, reason);
+
+        if (!result.IsSuccess)
+        {
+            return result;
+        }
+
+        return (Result)await _interactions.EditOriginalInteractionResponseAsync
+        (
+            _context.Interaction.ApplicationID,
+            _context.Interaction.Token,
+            "User kicked successfully."
+        );
+    }
+}
--- src/Kobalt/Plugins/Kobalt.Plugins.Infractions/InfractionPlugin.cs
+++ src/Kobalt/Plugins/Kobalt.Plugins.Infractions/InfractionPlugin.cs
@@ -1,5 +1,9 @@
 ﻿using Kobalt.Plugins.Infractions;
+using Kobalt.Plugins.Infractions.Commands;
+using Kobalt.Plugins.Infractions.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Remora.Commands.Extensions;
 using Remora.Plugins.Abstractions;
 using Remora.Plugins.Abstractions.Attributes;
 using Remora.Results;
@@ -13,8 +17,16 @@ public class InfractionPlugin : IPluginDescriptor
     public string Name => "Kobalt Infractions";
     public string Description => "Adds infractions to Kobalt.";
     public Version Version { get; } = new(0, 0, 1);
-    
-    public Result ConfigureServices(IServiceCollection serviceCollection) => default;
+
+    public Result ConfigureServices(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddHttpClient("Infractions", (s, c) => c.BaseAddress = new(s.GetService<IConfiguration>()!["Plugins:Infractions:ApiUrl"]!));
+        serviceCollection.AddSingleton<InfractionAPIService>();
+
+        serviceCollection.AddCommandTree().WithCommandGroup<KickCommand>();
+
+        return Result.FromSuccess();
+    }
 
     public async ValueTask<Result> InitializeAsync(IServiceProvider serviceProvider, CancellationToken ct = default) => default;
 }
