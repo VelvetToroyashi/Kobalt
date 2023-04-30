2d555795f4027fe9e8613bff349cad107c063170
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-04-29 21:43:24 -0400

    feat: Add notifications for futureproofing

---
+++ src/Kobalt/Kobalt.Shared/MediatR/Moderation/BanUserNotification.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Remora.Discord.API.Abstractions.Objects;
+using Remora.Rest.Core;
+
+namespace Kobalt.Shared.Mediator.Moderation;
+
+/// <summary>
+/// Represents the intention to ban a user; the user may or not have already been banned.
+/// </summary>
+/// <param name="GuildID">The ID of the guild of the event.</param>
+/// <param name="Target">The target of the ban.</param>
+/// <param name="Moderator">The moderator of the ban.</param>
+/// <param name="Reason">The reason for the ban.</param>
+/// <param name="Expiration">Then the ban expires, if ever.</param>
+public record BanUserNotification(Snowflake GuildID, IUser Target, IUser Moderator, string Reason, TimeSpan? Duration) : INotification;
---
+++ src/Kobalt/Plugins/Kobalt.Plugins.Infractions/MediatR/BanNotificationHandler.cs
@@ -0,0 +1,23 @@
+﻿using Kobalt.Plugins.Infractions.Services;
+using Kobalt.Shared.Mediator.Moderation;
+using MediatR;
+
+namespace Kobalt.Plugins.Infractions.MediatR;
+
+public class BanNotificationHandler : INotificationHandler<BanUserNotification>
+{
+    private readonly InfractionAPIService _infractions;
+    public BanNotificationHandler(InfractionAPIService infractions) => _infractions = infractions;
+
+    public async Task Handle(BanUserNotification notification, CancellationToken cancellationToken)
+    {
+        await _infractions.BanUserAsync
+        (
+            notification.GuildID,
+            notification.Target,
+            notification.Moderator,
+            notification.Reason,
+            notification.Duration
+        );
+    }
+}
