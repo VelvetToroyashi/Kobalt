cb682f3a25ab0143bc7d3781ea31786cf6381ed0
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-05-04 21:16:48 -0400

    feat: Add support for feedback messages

---
+++ src/Kobalt/Kobalt.Shared/Results/FeedbackResult.cs
@@ -0,0 +1,11 @@
+﻿using Remora.Discord.API.Abstractions.Objects;
+using Remora.Rest.Core;
+
+namespace Kobalt.Shared.Results;
+
+/// <summary>
+/// Represents a message to be sent to the user.
+/// </summary>
+/// <param name="Message">String content to send.</param>
+/// <param name="Embeds">Any embeds to send.</param>
+public record FeedbackResult(Optional<string> Message = default, Optional<IEnumerable<IEmbed>> Embeds = default);
