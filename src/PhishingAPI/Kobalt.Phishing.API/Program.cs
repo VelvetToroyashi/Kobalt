7356bfbf5aa28938dc78c163b38b1788104654eb
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-05-19 19:35:51 -0400

    feat: Implement endpoints for phishing service

--- src/PhishingAPI/Kobalt.Phishing.API/Program.cs
+++ src/PhishingAPI/Kobalt.Phishing.API/Program.cs
@@ -1,7 +1,11 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using Kobalt.Phishing.API.Services;
+using Kobalt.Phishing.Data.MediatR;
 using Kobalt.Phishing.Shared.Models;
 using Kobalt.Shared.Extensions;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Remora.Discord.API;
 using Remora.Rest.Json;
@@ -23,11 +27,53 @@ builder.Services
        .Configure(configure)
        .ConfigureHttpJsonOptions(opt => configure(opt.SerializerOptions));
 
+builder.Services
+       .AddSingleton<PhishingService>()
+       .AddHostedService(s => s.GetRequiredService<PhishingService>());
+
 var app = builder.Build();
 
-app.MapPost("/phishing/check/{guildID}/user", (ulong guildID, [FromBody] CheckUserRequest user) => new { });
-app.MapPost("/phishing/check/{guildID}/domains", (ulong guildID, [FromBody] IReadOnlyList<string> domains) => { });
-app.MapPut("/phishing/{guildID}/username", (ulong guildID, [FromBody] SubmitUsernameRequest request) => new { });
-app.MapPut("/phishing/{guildID}/avatar", (ulong guildID, [FromBody] SubmitAvatarRequest request) => new { });
+app.MapPost("/phishing/check/{guildID}/user", async (ulong guildID, [FromBody] CheckUserRequest user, PhishingService phishing) => await phishing.CheckUserAsync(guildID, user));
+app.MapPost("/phishing/check/domains", ([FromBody] IReadOnlyList<string> domains, PhishingService phishing) => phishing.CheckLinksAsync(domains));
+app.MapPut
+(
+    "/phishing/{guildID}/username",
+    async (ulong guildID, [FromBody] SubmitUsernameRequest request, IMediator mediator) =>
+    {
+        var res = await mediator.Send(new CreateSuspiciousUsername.Request(guildID, request.UsernamePattern, request.ParseType));
+
+        if (res.IsSuccess)
+        {
+            return Results.Created();
+        }
+
+        return Results.BadRequest(new { message = res.Error.Message });
+    }
+);
+
+app.MapPut
+(
+    "/phishing/{guildID}/avatar",
+    async (ulong guildID, [FromBody] SubmitAvatarRequest request, IMediator mediator, PhishingService phishing) =>
+    {
+        var hashResult = await phishing.HashImageAsync(request.Url);
+
+        if (!hashResult.IsDefined(out var hash))
+        {
+            return Results.BadRequest(new { message = hashResult.Error!.Message });
+        }
+
+        var md5 = Regex.Match(request.Url, @"https?:\/\/cdn\.discordapp\.com\/avatars\/[0-9]{17,19}\/(a_?)(?<Hash>\S+)\.png").Groups["Hash"].Value;
+
+        var res = await mediator.Send(new CreateSuspiciousAvatar.Request(guildID, md5, request.AddedBy.Value, request.Url, request.Category, hash));
+
+        if (res.IsSuccess)
+        {
+            return Results.Created();
+        }
+
+        return Results.BadRequest(res.Error);
+    }
+);
 
 app.Run();
