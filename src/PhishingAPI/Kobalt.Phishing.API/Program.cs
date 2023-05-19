9a201cc09f88c376912930b3c7b0951770c553bd
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-05-18 22:24:52 -0400

    feat: Add context and entities for phishing

--- src/PhishingAPI/Kobalt.Phishing.API/Program.cs
+++ src/PhishingAPI/Kobalt.Phishing.API/Program.cs
@@ -1,10 +1,27 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Kobalt.Phishing.Shared.Models;
 using Kobalt.Shared.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Remora.Discord.API;
+using Remora.Rest.Json;
+using Remora.Rest.Json.Policies;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
 builder.Services.AddSerilogLogging();
+
+var configure = (JsonSerializerOptions options) =>
+{
+    options.DefaultIgnoreCondition = JsonIgnoreCondition.Never;
+    options.PropertyNamingPolicy = new SnakeCaseNamingPolicy();
+    options.Converters.Insert(0, new SnowflakeConverter(Constants.DiscordEpoch));
+    options.Converters.Insert(1, new ISO8601DateTimeOffsetConverter());
+};
+
+builder.Services
+       .Configure(configure)
+       .ConfigureHttpJsonOptions(opt => configure(opt.SerializerOptions));
 
 var app = builder.Build();
 
--- src/PhishingAPI/Kobalt.Phishing.Data/PhishingContext.cs
+++ src/PhishingAPI/Kobalt.Phishing.Data/PhishingContext.cs
@@ -11,4 +11,12 @@ public class PhishingContext : DbContext
     public DbSet<SuspiciousAvatar> SuspiciousAvatars { get; set; } = null!;
 
     public DbSet<SuspiciousUsername> SuspiciousUsernames { get; set; } = null!;
+
+    public PhishingContext(DbContextOptions<PhishingContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("kobalt_phishing");
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(PhishingContext).Assembly);
+    }
 }
