55c18f87604a89ccca4e5381ec1b4dc362a80af1
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-05-20 05:37:45 -0400

    refactor!: Change SubmitUsernameRequest to record

--- src/PhishingAPI/Kobalt.Phishing.Shared/Models/SubmitUsernameRequest.cs
+++ src/PhishingAPI/Kobalt.Phishing.Shared/Models/SubmitUsernameRequest.cs
@@ -1,24 +1,10 @@
-using System.Text.Json.Serialization;
-
 namespace Kobalt.Phishing.Shared.Models;
 
 /// <summary>
 /// Represents a request to upload a username to the phishing database.
 /// </summary>
-public class SubmitUsernameRequest
-{
-    /// <summary>
-    /// The pattern to match usernames against.
-    /// </summary>
-    [JsonPropertyName("username_pattern")]
-    public string UsernamePattern { get; set; }
-
-    /// <summary>
-    /// Represents how the username should be parsed.
-    /// </summary>
-    [JsonPropertyName("parse_type")]
-    public UsernameParseType ParseType { get; set; }
-}
+/// <param name="UsernamePattern">The pattern to match usernames against.</param>
+public record SubmitUsernameRequest (string UsernamePattern, UsernameParseType ParseType);
 
 /// <summary>
 /// Dictates how usernames should be parsed.
