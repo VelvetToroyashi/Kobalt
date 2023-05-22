b901a86ab3149f1d2bcb4078a85629ef81d8068f
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-05-22 05:17:00 -0400

    feat: Migration for anti-phishing

---
+++ src/Kobalt/Plugins/Core/Kobalt.Plugins.Core.Data/Migrations/20230522091616_AddAntiPhishing.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Kobalt.Plugins.Core.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAntiPhishing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "guild_phishing_config",
+                schema: "kobalt_core",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    guild_id = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    scan_links = table.Column<bool>(type: "boolean", nullable: false),
+                    scan_users = table.Column<bool>(type: "boolean", nullable: false),
+                    detection_action = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_guild_phishing_config", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_guild_phishing_config_guilds_guild_id",
+                        column: x => x.guild_id,
+                        principalSchema: "kobalt_core",
+                        principalTable: "guilds",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_guild_phishing_config_guild_id",
+                schema: "kobalt_core",
+                table: "guild_phishing_config",
+                column: "guild_id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "guild_phishing_config",
+                schema: "kobalt_core");
+        }
+    }
+}
