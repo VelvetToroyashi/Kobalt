50fa15e27a16bc1c25f5a322eb826fcce31475a9
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2023-05-22 05:16:51 -0400

    fix!: Fix phishing config guild FK

--- src/Kobalt/Plugins/Core/Kobalt.Plugins.Core.Data/Entities/GuildPhishingConfig.cs
+++ src/Kobalt/Plugins/Core/Kobalt.Plugins.Core.Data/Entities/GuildPhishingConfig.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
 using Kobalt.Infractions.Shared;
+using Remora.Rest.Core;
 
 namespace Kobalt.Plugins.Core.Data.Entities;
 
@@ -9,12 +10,13 @@ namespace Kobalt.Plugins.Core.Data.Entities;
 public class GuildPhishingConfig
 {
     public int Id { get; set; }
-    public ulong GuildId { get; set; }
+    public required Snowflake GuildID { get; set; }
+
+    [NotNull]
+    public Guild? Guild { get; set; }
 
     public bool ScanLinks { get; set; }
     public bool ScanUsers { get; set; }
     public InfractionType DetectionAction { get; set; }
 
-    [NotNull]
-    public Guild? Guild { get; set; }
 }
