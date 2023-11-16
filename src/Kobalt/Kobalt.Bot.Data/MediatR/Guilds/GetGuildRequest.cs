7a3c12c3654cda9d3d276c40483e65f846f4f0b1
Velvet <42438262+VelvetToroyashi@users.noreply.github.com>
2023-11-16 18:30:25 -0500

    feat: Get all guild configs request

--- src/Kobalt/Kobalt.Bot.Data/MediatR/Guilds/GetGuildRequest.cs
+++ src/Kobalt/Kobalt.Bot.Data/MediatR/Guilds/GetGuildRequest.cs
@@ -1,4 +1,35 @@
-﻿namespace Kobalt.Bot.Data.MediatR.Guilds;
+﻿using Kobalt.Bot.Data.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Remora.Rest.Core;
+using Remora.Results;
+
+namespace Kobalt.Bot.Data.MediatR.Guilds;
 
 public static partial class GetGuild
-{ }
+{
+    public record Request(Snowflake GuildID) : IRequest<Result<Guild>>;
+    
+    internal class GetGuildHandler(IDbContextFactory<KobaltContext> context) : IRequestHandler<Request, Result<Guild>>
+    {
+        public async Task<Result<Guild>> Handle(Request request, CancellationToken cancellationToken)
+        {
+            await using var db = await context.CreateDbContextAsync(cancellationToken);
+
+            var guild = await db.Guilds
+                .Where(x => x.ID == request.GuildID)
+                .Include(x => x.AutoModConfig)
+                .Include(x => x.AntiRaidConfig)
+                .Include(x => x.PhishingConfig)
+                .Include(x => x.LogChannels)
+                .FirstOrDefaultAsync(cancellationToken);
+
+            if (guild is null)
+            {
+                return new NotFoundError($"No guild with the ID `{request.GuildID}` was found.");
+            }
+
+            return guild;
+        }
+    }
+}
