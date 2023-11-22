5487b0c7658f8278b040fe07035dca0fca6930eb
Velvet <42438262+VelvetToroyashi@users.noreply.github.com>
2023-11-22 01:27:11 -0500

    fix(bot): Make DTOs mutable

--- src/Kobalt/Kobalt.Bot.Data/DTOs/GuildAutoModConfigDTO.cs
+++ src/Kobalt/Kobalt.Bot.Data/DTOs/GuildAutoModConfigDTO.cs
@@ -2,5 +2,5 @@ namespace Kobalt.Bot.Data.DTOs;
 
 public class GuildAutoModConfigDTO(int? PushToTalkThreshold)
 {
-    public int? PushToTalkThreshold { get; init; } = PushToTalkThreshold;
+    public int? PushToTalkThreshold { get; set; } = PushToTalkThreshold;
 }
--- src/Kobalt/Kobalt.Bot.Data/DTOs/RoleMenuDTO.cs
+++ src/Kobalt/Kobalt.Bot.Data/DTOs/RoleMenuDTO.cs
@@ -2,9 +2,9 @@ namespace Kobalt.Bot.Data.DTOs;
 
 public class RoleMenuDTO(int Id, string Name, string Description, int MaxSelections, IReadOnlyList<RoleMenuOptionDTO> Options)
 {
-    public int Id { get; init; } = Id;
-    public string Name { get; init; } = Name;
-    public string Description { get; init; } = Description;
-    public int MaxSelections { get; init; } = MaxSelections;
-    public IReadOnlyList<RoleMenuOptionDTO> Options { get; init; } = Options;
+    public int Id { get; set; } = Id;
+    public string Name { get; set; } = Name;
+    public string Description { get; set; } = Description;
+    public int MaxSelections { get; set; } = MaxSelections;
+    public IReadOnlyList<RoleMenuOptionDTO> Options { get; set; } = Options;
 }
