b4fc2c9e6104ba9faf0b24aae7cc876add283eed
Velvet <42438262+VelvetToroyashi@users.noreply.github.com>
2023-11-29 10:14:53 -0500

    feat: Add `FromEntity` methods for role menus

--- src/Kobalt/Kobalt.Bot.Data/DTOs/RoleMenuDTO.cs
+++ src/Kobalt/Kobalt.Bot.Data/DTOs/RoleMenuDTO.cs
@@ -1,3 +1,24 @@
+using Kobalt.Bot.Data.Entities.RoleMenus;
+
 namespace Kobalt.Bot.Data.DTOs;
 
-public record RoleMenuDTO(int Id, string Name, string Description, int MaxSelections, IReadOnlyList<RoleMenuOptionDTO> Options);
+public record RoleMenuDTO
+(
+    int Id,
+    string Name,
+    string Description,
+    int MaxSelections,
+    IReadOnlyList<RoleMenuOptionDTO> Options
+)
+{
+    public static RoleMenuDTO FromEntity(RoleMenuEntity entity)
+    {
+        return new RoleMenuDTO(
+            entity.Id,
+            entity.Name,
+            entity.Description,
+            entity.MaxSelections,
+            entity.Options.Select(RoleMenuOptionDTO.FromEntity).ToList()
+        );
+    }
+}
