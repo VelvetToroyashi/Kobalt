5507f5c18d9417611ae829b9cebc8115ed55baaf
Velvet <42438262+VelvetToroyashi@users.noreply.github.com>
2024-01-21 07:55:12 -0500

    fix(tests): Fix more tests

--- tests/Kobalt.Infractions.Data.Tests/GetAllInfractionsTests.cs
+++ tests/Kobalt.Infractions.Data.Tests/GetAllInfractionsTests.cs
@@ -115,6 +115,6 @@ public class GetAllInfractionsTests
 
         var res = await handler.Handle(new(), default);
         
-        Assert.IsEmpty(res);
+        Assert.That(res, Is.Empty);
     }
 }
