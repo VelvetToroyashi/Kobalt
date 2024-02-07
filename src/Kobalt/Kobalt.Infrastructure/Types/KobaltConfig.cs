4d36b36c8016c32fe8a092ac7c2f7b6ab6af299e
Velvet <42438262+VelvetToroyashi@users.noreply.github.com>
2024-02-06 19:06:42 -0500

    feat(bot): Expand Kobalt's static configuratioin

--- src/Kobalt/Kobalt.Infrastructure/Types/KobaltConfig.cs
+++ src/Kobalt/Kobalt.Infrastructure/Types/KobaltConfig.cs
@@ -2,7 +2,35 @@
 
 public sealed class KobaltConfig
 {
+    public required KobaltBotConfig Bot { get; set; }
     public required KobaltDiscordConfig Discord { get; set; }
 };
 
+/// <summary>
+/// Represents the configuration for Discord.
+/// </summary>
+/// <param name="Token">The bot's token.</param>
+/// <param name="ShardCount">The amount of shards the bot will run.</param>
+/// <param name="PublicKey">The bot's public key for verifying interactions.</param>
 public record KobaltDiscordConfig(string Token, int ShardCount, string? PublicKey);
+
+/// <summary>
+/// Represents the configuration for the bot.
+/// </summary>
+/// <param name="RemindersUrl">The API URL for reminders.</param>
+/// <param name="PhishingUrl">The API URL for phishing.</param>
+/// <param name="InfractionsUrl">The API URL for infractions.</param>
+/// <param name="EnableReminders">Whether reminders are enabled.</param>
+/// <param name="EnablePhishing">Whether phishing is enabled.</param>
+/// <param name="EnableInfractions">Whether infractions are enabled.</param>
+/// <param name="EnableHTTPInteractions">Whether HTTP interactions are enabled.</param>
+public record KobaltBotConfig
+(
+    string? RemindersUrl = null,
+    string? PhishingUrl = null,
+    string? InfractionsUrl = null,
+    bool EnableReminders = true,
+    bool EnablePhishing = true,
+    bool EnableInfractions = true,
+    bool EnableHTTPInteractions = true
+);
